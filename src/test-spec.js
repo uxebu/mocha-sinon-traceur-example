8de69c5a1efb240cdc15abca3070018865b0a9ce
Wolfram Kriesing <wk@uxebu.com>
2014-11-19 18:06:44 +0100

    Some more ES6ify.

--- src/test-spec.js
+++ src/test-spec.js
@@ -1,15 +1,16 @@
-import {Some} from './test'
+import {Some} from './some'
 var assert = require('assert');
 require('./sinon-cleanup');
 
 describe('something', () => {
+
   it('that should work', () => {
     assert.equal(new Some().method(), 'works');
   });
 
   it('should have called', function() {
-    var some = new Some();
-    var stub = this.sinon.stub(some, 'value');
+    let some = new Some();
+    let stub = this.sinon.stub(some, 'value');
     stub.returns(42);
 
     assert.equal(some.method(), 42);
