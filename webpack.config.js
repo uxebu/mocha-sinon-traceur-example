ba8d321f7b413881cbd27a68d0db24b1a8d71956
Wolfram Kriesing <wk@uxebu.com>
2014-11-20 11:35:51 +0100

    Exclude the node_modules directory completely, so it won't screw up things like jquery, etc.

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,16 @@ module.exports = {
   module: {
     loaders: [
       // Transpile any JavaScript file:
-      { test: /\.js$/, loader: 'webpack-traceur?runtime&sourceMaps&experimental' }
+      {
+        test: /\.js$/,
+        loader: 'webpack-traceur?runtime',
+        exclude: /node_modules/
+      }
     ]
   },
   resolve: {
     // you can now require('file') instead of require('file.js')
     extensions: ['', '.js', '.json']
-  }
+  },
+  debug: true // shows also which files run through traceur, good to learn and validate the `exclude`
 };
